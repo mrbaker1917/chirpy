77e3ee6e3647c0e892944d2ee272f7a2f40fe904
mrbaker1917 <mrbaker1917@gmail.com>
2026-01-26 17:59:08 -0800

    adds better order to setting up default path for server

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -9,11 +10,15 @@ func main() {
 	serveMux := http.NewServeMux()
 	var httpServer http.Server
 	httpServer.Addr = ":8080"
+	fs := http.FileServer(http.Dir("."))
+	serveMux.Handle("/", fs)
 	httpServer.Handler = serveMux
+
+	fmt.Printf("Server is running on localhost%s\n", httpServer.Addr)
 	err := httpServer.ListenAndServe()
 
 	if err != nil {
-		fmt.Println("server could not be set up: ", err)
+		log.Println("server could not be set up: ", err)
 	}
 
 }
