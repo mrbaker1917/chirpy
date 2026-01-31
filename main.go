debe7fe0f5e930d45b46f55df9eca2f6a2679373
mrbaker1917 <mrbaker1917@gmail.com>
2026-01-31 12:33:56 -0800

    adds handler to validate chirp length

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,23 +12,53 @@ type apiConfig struct {
 	fileserverHits atomic.Int32
 }
 
-func main() {
-	const filepathRoot = "."
-	const port = "8080"
-	mux := http.NewServeMux()
-	cfg := apiConfig{}
-	mux.HandleFunc("GET /admin/metrics", cfg.handlerMetrics)
-	mux.HandleFunc("POST /admin/reset", cfg.handlerReset)
-	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
-	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+// helpers:
 
-	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: mux,
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
 	}
 
-	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
-	log.Fatal(srv.ListenAndServe())
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	payld := make(map[string]string)
+	payld["error"] = msg
+	respondWithJSON(w, code, payld)
+}
+
+// handlers:
+
+func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	type chirp struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	chp := chirp{}
+	err := decoder.Decode(&chp)
+	if err != nil {
+		log.Printf("Error decoding chirp: %s", err)
+		respondWithError(w, 500, "Error decoding request body")
+		return
+	}
+
+	type repbody struct {
+		Valid bool `json:"valid"`
+	}
+
+	if len(chp.Body) > 140 {
+		respondWithError(w, 400, "Chirp is too long")
+		return
+	}
+
+	respondWithJSON(w, 200, repbody{Valid: true})
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -61,4 +92,24 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+	mux := http.NewServeMux()
+	cfg := apiConfig{}
+	mux.HandleFunc("GET /admin/metrics", cfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", cfg.handlerReset)
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 }
